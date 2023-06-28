2295f0cdf483ea5c044769c2e28edec13b5a2dfa
ibrahimhamed112 <ibrahim.hamed112@hotmail.com>
2023-06-28 10:03:23 +0300

    add new screens

--- screens/ContactusScreen.js
+++ screens/ContactusScreen.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 
-export default class CheckoutScreen extends Component {
+export default class ContactusScreen extends Component {
     render() {
         return (
             <View>
